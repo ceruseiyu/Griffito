79a86517af9dbeed65183915e2f8340b4cbd9978
Griffito <eurocracy2@gmail.com>
2017-09-10 18:04:42 +0000

    Initial Commit

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Oliver Griffiths - Griffito</title>
+        <link href="https://fonts.googleapis.com/css?family=Rubik" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="css/bulma.css" />
+        <link rel="stylesheet" type="text/css" href="css/griffito.css" />
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+    </head>
+    <body>
+        <header>
+            <nav class="navbar">
+                <div class="navbar-brand">
+                    <a class="navbar-item" href="http://griffito.info">
+                        <span class="logo-font">Griffito</span>
+                    </a>
+                    <a class="navbar-item is-hidden-desktop" href="https://github.com/ceruseiyu">
+                        <span class="icon" style="color: #333;">
+                            <i class="fa fa-github"></i>
+                        </span>
+                    </a>
+                    <a class="navbar-item is-hidden-desktop" href="https://www.linkedin.com/in/oliver-griffiths-9853a214a/">
+                        <span class="icon" style="color: #333;">
+                            <i class="fa fa-linkedin-square"></i>
+                        </span>
+                    </a>
+                    <div class="navbar-burger">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+                <div class="navbar-menu">
+                    <div class="navbar-start">
+                        <a class="navbar-item" href="projects.php">
+                            Projects
+                        </a>
+                        <a class="navbar-item" href="about.php">
+                            About
+                        </a>
+                        <a class="navbar-item" href="contact.php">
+                            Contact
+                        </a>
+                    </div>
+                    <div class="navbar-end">
+                        <a class="navbar-item is-hidden-desktop-only" href="https://github.com/ceruseiyu">
+                            <span class="icon" style="color: #333;">
+                                <i class="fa fa-github"></i>
+                            </span>
+                        </a>
+                        <a class="navbar-item is-hidden-desktop-only" href="https://www.linkedin.com/in/oliver-griffiths-9853a214a/">
+                            <span class="icon" style="color: #333;">
+                                <i class="fa fa-linkedin-square"></i>
+                            </span>
+                        </a>
+                        <div class="navbar-item">
+                            <div class="field is-grouped">
+                                <p class="control">
+                                    <a class="button" href="login.php">
+                                        <span class="icon">
+                                            <i class="fa fa-sign-in"></i>
+                                        </span>
+                                        <span>Log in</span>
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <section class="section">
+            <div class="container">
+                <h1 class="title">Title</h1>
+                <p class="subtitle">Content <strong>Strong</strong></p>
+            </div>
+      </section>
+    </body>
+</html>
