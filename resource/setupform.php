e4a04f5f96d588919f35ceec0dc15001f81d23c6
Griffito <eurocracy2@gmail.com>
2017-09-12 20:16:20 +0000

    Added setupform

---
+++ resource/setupform.php
@@ -0,0 +1,5 @@
+<form action="" method="post">
+    <label>Enter the password for the admin user:</label><br />
+    <input type="password" name="password">
+    <input name="submit" type="submit" value="Submit">
+</form>
